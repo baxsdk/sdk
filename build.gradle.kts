1420592be7738a2ea90e1c502e8f3d6468f3175d
Alex Sagastegui <alxsags@gmail.com>
2025-01-29 13:17:15 -0600

    Add Licence and modify  JitPack release

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+val sdkTag = "0.0.2"
+
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
@@ -5,7 +7,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.baxsdk"
+    namespace = "mx.bax.sdk.BaxSDK"
     compileSdk = 34
 
     defaultConfig {
@@ -20,8 +22,14 @@ android {
     }
 }
 
+repositories {
+    mavenCentral()
+    google()
+    maven("https://jitpack.io")
+}
+
 dependencies {
-    implementation 'com.github.baxsdk:sdk:Tag'
+    implementation("com.github.baxsdk:sdk:$sdkTag")
 }
 
 publishing {
@@ -29,9 +37,38 @@ publishing {
         create<MavenPublication>("release") {
             from(components["release"])
 
-            groupId = "com.github.alxsags"
+            groupId = "com.github.baxsdk"
             artifactId = "BaxSDK"
-            version = "0.0.1"
+            version = "$sdkTag"
+
+            pom {
+                name.set("BaxSDK")
+                description.set("SDK for integrating with the Bax service")
+                url.set("https://github.com/baxsdk/sdk")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("baxsdk")
+                        name.set("Bax SDK Team")
+                        email.set("sdk@bax.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/baxsdk/sdk.git")
+                    developerConnection.set("scm:git:ssh://github.com/baxsdk/sdk.git")
+                    url.set("https://github.com/baxsdk/sdk")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            url = uri("https://jitpack.io")
         }
     }
 }
